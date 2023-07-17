f3745cdf8a8c0dcd732cf10334eb26f8b0af9c41
Yurii <sfiwty@gmail.com>
2023-07-17 22:57:24 +0300

    add services and conclusion

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ function serve() {
     browserSync.init({
         server: {
             baseDir: "./" + distPath,
-            index: 'specialists.html'
+            index: 'index.html'
         }
     });
 }

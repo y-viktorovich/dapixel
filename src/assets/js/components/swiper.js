6ec646c7a4b64546a2296e9fb4f43d6f7db3bcac
Yurii <sfiwty@gmail.com>
2023-07-26 02:12:53 +0300

    add full page adaptation

--- src/assets/js/components/swiper.js
+++ src/assets/js/components/swiper.js
@@ -4,19 +4,25 @@
 
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 
 
 const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Autoplay],
+
     // Optional parameters
     direction: 'horizontal',
     loop: true,
   
     // Navigation arrows
-    modules: [Navigation],
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 
+    autoplay: {
+      delay: 4000,
+    },
+    
     speed: 700,
 });
